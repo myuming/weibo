f47dfb0128b66516516736f57fdac13e2cbf25c5
MING <244264663@qq.com>
2019-11-22 09:38:49 +0000

    root-1911221738

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -19,6 +19,13 @@ class UsersController extends Controller
         ]);
     }
 
+    public function index()
+    {
+        $users = User::paginate(10);
+
+        return view('users.index', compact('users'));
+    }
+
     public function create()
     {
     	return view('users.create');
@@ -76,6 +83,16 @@ class UsersController extends Controller
         session()->flash('success', '个人资料更新成功！');
 
         return redirect()->route('users.show', $user->id);
+    }
+
+
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+
+        return back();
     }
 
 }
