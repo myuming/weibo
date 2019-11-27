4b6bddf246ef6677fe412c1d8d88c42444823be4
MING <244264663@qq.com>
2019-11-27 10:04:37 +0000

    root-1911271804

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,8 +54,27 @@ class User extends Authenticatable
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
 
+
     public function statuses()
     {
         return $this->hasMany(Status::class);
+    }
+
+
+    public function feed()
+    {
+        return $this->statuses()->orderBy('created_at', 'desc');
+    }
+
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
+    }
+
+
+    public function followings()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
     }
 }
